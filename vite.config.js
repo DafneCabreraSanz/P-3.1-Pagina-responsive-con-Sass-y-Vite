443f32a24a1f3c8872813b294d5fe2cf9ee20173
DafneCabreraSanz <alumno.712343@ies-azarquiel.es>
2025-11-20 10:49:36 +0100

    final

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
     root: "./src",
     build: {
         outDir: "../docs",
-        emptyOutDir: true,
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "./src/index.html"),
-
-            },
-        },
     },
     base: "./",
 });
